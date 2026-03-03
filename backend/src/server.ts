3a26dd646cd71fbced1ff663404b0414b27f5694
Yoav Segev <host-yoav@local.com>
2026-03-03 10:45:02 +0200

    finish ci cd

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import { errorMiddleware } from "./middlewares/error.middleware.js";
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use("/", listRouter);
 app.use("/", otherRouter);
