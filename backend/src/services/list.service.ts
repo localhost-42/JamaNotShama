8a0dd258d100a463759d8cc099d16d62bd1b0fdb
Yoav Segev <host-yoav@local.com>
2026-03-03 23:14:27 +0200

    fix(sercives/): Fix table name

--- backend/src/services/list.service.ts
+++ backend/src/services/list.service.ts
@@ -9,11 +9,10 @@ function isPgUniqueViolation(err: unknown): boolean {
   );
 }
 
-
 export const enterList = async (id: number): Promise<void> => {
   try {
     await pool.query<ListRow>(
-      "INSERT INTO list(user_id, exit_time) VALUES ($1, $2)",
+      "INSERT INTO jns.list(user_id, exit_time) VALUES ($1, $2)",
       [id, null],
     );
   } catch (err) {
@@ -27,7 +26,7 @@ export const enterList = async (id: number): Promise<void> => {
 export const exitList = async (id: number): Promise<void> => {
   try {
     await pool.query<ListRow>(
-      "UPDATE list SET exit_time = NOW() WHERE user_id = $1 AND exit_time IS NULL",
+      "UPDATE jns.list SET exit_time = NOW() WHERE user_id = $1 AND exit_time IS NULL",
       [id],
     );
   } catch (err) {
@@ -39,13 +38,11 @@ export const exitList = async (id: number): Promise<void> => {
   }
 };
 
-
 export const enterQueue = async (id: number): Promise<void> => {
   try {
-   await pool.query<ListRow>(
-      "INSERT INTO queue(user_id) VALUES ($1)",
-      [id]
-    );
+    await pool.query<ListRow>("INSERT INTO jns.queue(user_id) VALUES ($1)", [
+      id,
+    ]);
   } catch (err) {
     if (isPgUniqueViolation(err)) {
       throw new AppError("User is already in the queue", 400);
@@ -56,10 +53,7 @@ export const enterQueue = async (id: number): Promise<void> => {
 
 export const exitQueue = async (id: number): Promise<void> => {
   try {
-    await pool.query<ListRow>(
-      "DELETE FROM queue WHERE user_id = $1",
-      [id],
-    );
+    await pool.query<ListRow>("DELETE FROM jns.queue WHERE user_id = $1", [id]);
   } catch (err) {
     if (isPgUniqueViolation(err)) {
       throw new AppError("User is already out of the queue", 400);
@@ -69,12 +63,10 @@ export const exitQueue = async (id: number): Promise<void> => {
   }
 };
 
-
-
 export const getList = async (): Promise<string[]> => {
   try {
     const r = await pool.query<ListRow & UserRow>(
-      "SELECT u.name FROM list l JOIN users u ON l.user_id = u.user_id WHERE l.exit_time IS NULL ORDER BY l.enter_time ASC",
+      "SELECT u.name FROM jns.list l JOIN jns.users u ON l.user_id = u.user_id WHERE l.exit_time IS NULL ORDER BY l.enter_time ASC",
     );
     return r.rows.map((row) => row.name);
   } catch (err) {
@@ -82,19 +74,19 @@ export const getList = async (): Promise<string[]> => {
       return [];
     }
     throw err;
-  } 
+  }
 };
 
 export const getQueue = async (): Promise<string[]> => {
   try {
     const r = await pool.query<ListRow & UserRow>(
-      "SELECT u.name FROM queue q JOIN users u ON q.user_id = u.user_id WHERE q.exit_time IS NULL ORDER BY q.enter_time ASC",
+      "SELECT u.name FROM jns.queue q JOIN jns.users u ON q.user_id = u.user_id WHERE q.exit_time IS NULL ORDER BY q.enter_time ASC",
     );
     return r.rows.map((row) => row.name);
   } catch (err) {
     if (isPgUniqueViolation(err)) {
       return [];
-    }   
-    throw err;  
+    }
+    throw err;
   }
 };
