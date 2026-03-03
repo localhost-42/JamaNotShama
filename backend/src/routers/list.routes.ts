838bef8b3cd651afc5384db938fc02854b0947a8
vegennoodles <guyneimark@gmail.com>
2026-03-03 20:33:21 +0200

    fix(router) fix directories a bit

--- backend/src/routers/list.routes.ts
+++ backend/src/routers/list.routes.ts
@@ -7,7 +7,7 @@ import {
   getListHandler,
   getQueueHandler,
 } from "../controllers/list.controller.js";
-
+import { login, logs, excelHandler } from "../controllers/other.controller.js";
 const router = Router();
 
 router.post("/enter/:id/queue", enterQueueHandler);
@@ -16,5 +16,9 @@ router.delete("/exit/:id/queue", exitQueueHandler);
 router.put("/exit/:id/list", exitListHandler);
 router.get("/queue", getQueueHandler);
 router.get("/list", getListHandler);
+router.post("/login", login);
+router.get("/logs", logs);
+router.get('/excel', excelHandler)
+
 
 export default router;
