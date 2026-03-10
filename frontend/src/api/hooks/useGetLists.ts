e496bcf8990e9aaa8a577f1fc9de10c43c52b611
Yoav Segev <host-yoav@local.com>
2026-03-10 21:17:28 +0200

    fix(useGetLists.ts): Fix exit list button

--- frontend/src/api/hooks/useGetLists.ts
+++ frontend/src/api/hooks/useGetLists.ts
@@ -1,31 +1,17 @@
 import { useEffect, useState } from "react";
 import api from "..";
 import { socket } from "../socket";
+import type { nameTimeRow } from "../../utils/types";
 
 export const useGetLists = () => {
-  const [peopleOutside, setPeopleOutside] = useState<string[]>([]);
-
-  const formatTime = (dateValue: string | Date): string => {
-    const date = new Date(dateValue);
-
-    return `${date.getHours().toString().padStart(2, "0")}:${date
-      .getMinutes()
-      .toString()
-      .padStart(2, "0")}`;
-  };
+  const [peopleOutside, setPeopleOutside] = useState<nameTimeRow[]>([]);
 
   const fetchList = async () => {
     api
       .list()
       .getAll()
       .then((response) => {
-        setPeopleOutside(
-          response.data.length
-            ? response.data.map(
-                ({ name, enterTime }) => `${name}  ${formatTime(enterTime)}`,
-              )
-            : [],
-        );
+        setPeopleOutside(response.data ?? []);
       })
       .catch((error) => {
         alert("Error fetching list:" + error.message);
